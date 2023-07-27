a968fdc74e3448ac20a4b98c60c9566f34d32e26
Nghĩa Trịnh <85700756+nghiatn1609@users.noreply.github.com>
2023-07-27 15:33:27 +0700

    Update oder_product.py

--- custommodule/oder_product/models/oder_product.py
+++ custommodule/oder_product/models/oder_product.py
@@ -8,29 +8,29 @@ class oder_product(models.Model):
     _description = 'Yêu cầu mua hàng'
 
 
-    Department_id = fields.Many2one(comodel_name="hr.department", string="Department")
-    Request_id = fields.Many2one('res.users', string="Request By")
-    Approver_id = fields.Many2one('res.users', string="Approved By")
-    Date = fields.Date(default=fields.Date.today)
-    Date_approve = fields.Date(string="Date Approve")
-    Request_line_ids = fields.One2many(comodel_name="purchase.request.line", inverse_name='request_id',string='Request by who')
-    Description = fields.Text()
-    State = fields.Selection([('draft','draft'),
+    department_id = fields.Many2one(comodel_name="hr.department", string="Department")
+    request_id = fields.Many2one('res.users', string="Request By")
+    approver_id = fields.Many2one('res.users', string="Approved By")
+    date = fields.Date(default=fields.Date.today)
+    date_approve = fields.Date(string="Date Approve")
+    request_line_ids = fields.One2many(comodel_name="purchase.request.line", inverse_name='request_id',string='Request by who')
+    description = fields.Text()
+    state = fields.Selection([('draft','draft'),
                               ('wait','wait'),
                               ('approve','approve'),
                               ('cancel','cancel')],
                             string="State", help="State of request order", default='draft')
-    Total_qty = fields.Float(string="Total Quantity", compute="_get_total_qty")
-    Total_Amount = fields.Float(string="Total Amount", compute ="_get_total_amount")
+    total_qty = fields.Float(string="Total Quantity", compute="_get_total_qty")
+    total_Amount = fields.Float(string="Total Amount", compute ="_get_total_amount")
     
     
-    @api.depends('Request_line_ids.Qty')
+    @api.depends('request_line_ids.qty')
     def _get_total_qty(self):
         for r in self:
-           r.Total_qty += r.Request_line_ids.Qty
+           r.total_qty += r.request_line_ids.qty
                 
     
-    @api.depends('Request_line_ids.Total')
+    @api.depends('request_line_ids.total')
     def _get_total_amount(self):
         for r in self:
-            r.Total_Amount += r.Request_line_ids.Total 
+            r.total_Amount += r.request_line_ids.total 
