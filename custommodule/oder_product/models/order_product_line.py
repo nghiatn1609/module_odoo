579e816fc0a8f8597467b7aefbd46115a01d6d5f
Nghĩa Trịnh <85700756+nghiatn1609@users.noreply.github.com>
2023-07-27 15:34:00 +0700

    Update order_product_line.py

--- custommodule/oder_product/models/order_product_line.py
+++ custommodule/oder_product/models/order_product_line.py
@@ -11,11 +11,11 @@ class oder_product_line(models.Model):
     request_id = fields.Many2one(comodel_name="purchase.request", string="Request By")
     product_id = fields.Many2one(comodel_name="product.template", string="Product ID")
     uom_id = fields.Many2one(comodel_name="uom.uom",string="Unit of Measure")
-    Qty= fields.Float()
-    Qty_approve = fields.Float()
-    Total = fields.Float(string="Total", compute="_total")
+    qty= fields.Float()
+    qty_approve = fields.Float()
+    total = fields.Float(string="Total", compute="_total")
     
-    @api.depends('product_id.list_price','Qty')
+    @api.depends('product_id.list_price','qty')
     def _total(self):
         for r in self:
-            r.Total =  r.Qty * r.product_id.list_price
+            r.total =  r.qty * r.product_id.list_price
