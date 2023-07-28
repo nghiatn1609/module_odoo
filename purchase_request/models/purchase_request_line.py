ec5c73ca03cef0d728ce336d2ca25e65dd638a88
Nghĩa Trịnh <85700756+nghiatn1609@users.noreply.github.com>
2023-07-28 09:52:00 +0700

    Update purchase_request_line.py

--- purchase_request/models/purchase_request_line.py
+++ purchase_request/models/purchase_request_line.py
@@ -15,19 +15,9 @@ class purchase_request_line(models.Model):
     qty_approve = fields.Float()
     total = fields.Float(string="Total", compute="_total")
     
-    # price_unit = fields.Float(string='Unit Price', compute='_compute_price_unit', store=True)
     
     @api.depends('product_id.list_price','qty')
     def _total(self):
         for r in self:
             r.total =  r.qty * r.product_id.list_price
 
-
-    # @api.depends('product_id', 'product_id.seller_ids')
-    # def _compute_price_unit(self):
-    #     for line in self:
-    #         supplier_info = line.product_id.seller_ids.filtered(lambda r: r.name.id == line.request_id.partner_id.id)
-    #         if supplier_info:
-    #             line.price_unit = supplier_info[0].price
-    #         else:
-    #             line.price_unit = 0.0
